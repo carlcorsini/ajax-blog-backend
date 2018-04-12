b701e4d2345d17e0a5b163f30cb229a75b0c6b62
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-04-12 16:41:56 -0700

    trying to asynch this bia

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,63 +1,65 @@
-const model = require("../models/main");
+const model = require('../models/main')
 
 finder = (array, paramId) => {
   return array.find(a => {
-    return a.id === paramId;
-  });
-};
+    return a.id === paramId
+  })
+}
 
 createPost = (req, res, next) => {
-  const result = model.createPost(req.body.title, req.body.content);
-  if (result.error) next(result);
+  const promise = model.createPost(req.body.title, req.body.content)
+  if (result.error) next(result)
   else
-    res.status(201).json({
-      result,
-      message: "item created"
-    });
-};
+    promise.then(result => {
+      res.status(201).json({
+        result,
+        message: 'item created'
+      })
+    })
+}
 
 getAllPosts = (req, res, next) => {
-  const result = model.getAllPosts(req.query.limit);
-  if (result.error) next(result);
+  const result = model.getAllPosts(req.query.limit)
+  if (result.error) next(result)
   else
     res.status(201).json({
       result,
-      message: "all posts returned"
-    });
-};
+      message: 'all posts returned'
+    })
+}
 
 getPostById = (req, res, next) => {
-  const result = model.getPostById(req.params.id);
-  if (result.error) next(result);
+  const result = model.getPostById(req.params.id)
+  if (result.error) next(result)
   else
     res.status(200).json({
       result,
       message: `Post with ID: ${req.params.id} returned`
-    });
-};
+    })
+}
 
 update = (req, res, next) => {
   const result = model.update(
     req.params.id,
     req.body.editTitle,
     req.body.editContent
-  );
-  if (result.error) next(result);
+  )
+  if (result.error) next(result)
   else
     res.status(200).json({
       result,
       message: `item with ID: ${req.params.id} updated`
-    });
-};
+    })
+}
 deletePost = (req, res, next) => {
-  const result = model.deletePost(req.params.id);
-  if (result.error) next(result);
+  const result = model.deletePost(req.params.id)
+  if (result.error) next(result)
   else
     res.status(200).json({
       result,
       message: `item with ID: ${req.params.id} deleted`
-    });
-};
+    })
+}
 
 module.exports = {
   createPost,
@@ -65,4 +67,4 @@ module.exports = {
   getPostById,
   update,
   deletePost
-};
+}
