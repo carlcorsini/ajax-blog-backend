e823148d203cf0a5d91aa72a9586e233db1593a6
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-04-12 14:18:05 -0700

    lots of stuff

--- app.js
+++ app.js
@@ -5,16 +5,6 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-var jsonServer = require("json-server");
-var server = jsonServer.create();
-var router = jsonServer.router("./db/db.json");
-var middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-server.listen(3000, function() {
-  console.log("JSON Server is running");
-});
 
 if (process.env.NODE_ENV !== "test") app.use(morgan("dev"));
 app.use(bodyParser.json());
