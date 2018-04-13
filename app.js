509c5a618e550552330a52bbc6fc276b940df286
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-04-12 21:07:26 -0700

    Changes

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const port = process.env.PORT || 3000;
-const cors = require("cors");
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+const cors = require('cors')
 
-if (process.env.NODE_ENV !== "test") app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(cors());
+if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
 
-const blogRoutes = require("./src/routes/main.js");
+const blogRoutes = require('./src/routes/main.js')
 
-app.use("/blog", blogRoutes);
+app.use('/blog', blogRoutes)
 
 app.use((err, req, res, next) => {
-  res.status(err.status).json(err);
-});
+  res.status(err.status).json(err)
+})
 
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
-    console.log(`Da blog is running on ${port}!`);
-  });
+    console.log(`Da blog is running on ${port}!`)
+  })
 }
-module.exports = app;
+module.exports = app
--- src/models/main.js
+++ src/models/main.js
@@ -1,100 +1,104 @@
-const db = "../../db";
-const uuid = require("uuid/v4");
-const path = require("path");
-const fs = require("fs");
-const dbPath = fs.readFileSync(path.join(__dirname, db, "db.json"));
-const dbPath2 = path.join(__dirname, db, "db.json");
+const database = '../../database'
+const uuid = require('uuid/v4')
+const path = require('path')
+const fs = require('fs')
+const dbPath = fs.readFileSync(path.join(__dirname, database, 'db.json'))
+const dbPath2 = path.join(__dirname, database, 'db.json')
 
 finder = (array, paramId) => {
   return array.find(a => {
-    return a.id === paramId;
-  });
-};
+    return a.id === paramId
+  })
+}
 
 createPost = (title, content) => {
-  let result;
+  let result
   if (!content || !title) {
     result = {
       status: 400,
-      error: "No content/title entered"
-    };
-    return result;
+      error: 'No content/title entered'
+    }
+    return result
   }
-  const postArray = JSON.parse(dbPath, "utf-8");
+  const postArray = JSON.parse(dbPath, 'utf-8')
   let newPost = {
     id: uuid(),
     title,
     content
-  };
-  postArray.push(newPost);
-  console.log("New Post === ", newPost);
-  console.log("Post Array === ", postArray);
-  fs.writeFileSync(dbPath2, JSON.stringify(postArray));
-  result = postArray[postArray.length - 1];
-  console.log("Result is??? === ", result);
-  return result;
-};
+  }
+  postArray.push(newPost)
+  console.log('New Post === ', newPost)
+  console.log('Post Array === ', postArray)
+  // fs.writeFileSync(dbPath2, JSON.stringify(postArray))
+  fs.writeFileSync(
+    path.join(__dirname, database, 'db.json'),
+    JSON.stringify(postArray)
+  )
+  result = postArray[postArray.length - 1]
+  console.log('Result is??? === ', result)
+  return result
+}
 
 getAllPosts = limit => {
-  const postArray = JSON.parse(dbPath, "utf-8");
-  console.log("what is in here??? == ", postArray);
-  const result = !limit ? postArray : postArray.slice(0, limit);
-  return result;
-};
+  const postArray = JSON.parse(dbPath, 'utf-8')
+  console.log('what is in here??? == ', postArray)
+  const result = !limit ? postArray : postArray.slice(0, limit)
+  return result
+}
 
 getPostById = paramId => {
-  let result;
-  const postArray = JSON.parse(dbPath, "utf-8");
+  let result
+  const postArray = JSON.parse(dbPath, 'utf-8')
   if (!finder(postArray, paramId)) {
     result = {
       status: 400,
-      error: "ID not found"
-    };
-    return result;
+      error: 'ID not found'
+    }
+    return result
   }
-  result = finder(postArray, paramId);
-  return result;
-};
+  result = finder(postArray, paramId)
+  return result
+}
 
 update = (paramId, title, content) => {
-  let result;
-  const postArray = JSON.parse(dbPath, "utf-8");
+  let result
+  const postArray = JSON.parse(dbPath, 'utf-8')
 
   if (!finder(postArray, paramId)) {
     result = {
       status: 400,
-      error: "ID not found"
-    };
-    return result;
+      error: 'ID not found'
+    }
+    return result
   }
   postArray.forEach((a, idx) => {
     if (a.id === paramId) {
-      postArray[idx].title = title;
-      postArray[idx].content = content;
-      result = postArray[idx];
+      postArray[idx].title = title
+      postArray[idx].content = content
+      result = postArray[idx]
     }
-  });
-  fs.writeFileSync(dbPath2, JSON.stringify(postArray));
-  return result;
-};
+  })
+  fs.writeFileSync(dbPath2, JSON.stringify(postArray))
+  return result
+}
 
 deletePost = paramId => {
-  const postArray = JSON.parse(dbPath, "utf-8");
+  const postArray = JSON.parse(dbPath, 'utf-8')
   if (!finder(postArray, paramId)) {
     result = {
       status: 400,
-      error: "ID not found"
-    };
-    return result;
+      error: 'ID not found'
+    }
+    return result
   }
   postArray.forEach((a, idx) => {
     if (a.id === paramId) {
-      postArray.splice(idx, 1);
+      postArray.splice(idx, 1)
     }
-  });
-  fs.writeFileSync(dbPath2, JSON.stringify(postArray));
-  return postArray;
-};
+  })
+  fs.writeFileSync(dbPath2, JSON.stringify(postArray))
+  return postArray
+}
 
 module.exports = {
   createPost,
@@ -102,4 +106,4 @@ module.exports = {
   getPostById,
   update,
   deletePost
-};
+}
